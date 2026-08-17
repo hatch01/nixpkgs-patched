a90ffc621b6fd7ba38ae90e78878d9b6480ca473
NixPkgs Patcher <patcher@nixpkgs.local>
2026-08-17 21:16:21 +0000

    Apply PR #553547: loadbox

---
+++ pkgs/by-name/lo/loadbox/package.nix
@@ -0,0 +1,61 @@
+{
+  cairo,
+  fetchFromGitHub,
+  freetype,
+  lib,
+  libx11,
+  libsndfile,
+  pkg-config,
+  stdenv,
+  xorgproto,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "loadbox";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "brummer10";
+    repo = "LoadBox";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-dYwJOazC/osLjfVsxbFa0lRMeFXhXCAZViEWQJEKJPE=";
+    fetchSubmodules = true;
+  };
+
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    cairo
+    freetype
+    libx11
+    libsndfile
+    xorgproto
+  ];
+
+  makeFlags = [
+    "PREFIX=$(out)"
+    "CLAP_INSTAL_DIR=$(out)/lib/clap"
+    "VST2_INSTAL_DIR=$(out)/lib/vst"
+    "VST3_INSTALL_DIR=$(out)/lib/vst3"
+    "user=root"
+    "STRIP=:"
+  ];
+
+  postPatch = ''
+    substituteInPlace LoadBox/makefile \
+      --replace-fail "-flto=auto" ""
+  '';
+
+  meta = {
+    homepage = "https://github.com/brummer10/LoadBox";
+    description = "Stereo IR / NAM plugin";
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ eymeric ];
+    platforms = lib.platforms.linux;
+  };
+})
