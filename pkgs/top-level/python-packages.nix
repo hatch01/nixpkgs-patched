6c1303d38825619c35fa731b0579ba4883a55789
NixPkgs Patcher <patcher@nixpkgs.local>
2026-08-19 10:27:33 +0000

    Apply PR #402608: satochip-utils

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14614,6 +14614,8 @@ self: super: with self; {
 
   pycryptodomex = callPackage ../development/python-modules/pycryptodomex { };
 
+  pycryptotools = callPackage ../development/python-modules/pycryptotools { };
+
   pycsdr = callPackage ../development/python-modules/pycsdr { };
 
   pycsspeechtts = callPackage ../development/python-modules/pycsspeechtts { };
