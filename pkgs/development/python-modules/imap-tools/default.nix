ac0dc4de5a8e222e9d168ee7688f9fd6e84be8f6
dotlambda <github@dotlambda.de>
2026-09-10 03:12:48 +0000

    [Backport release-26.05] postfix: 3.11.6 -> 3.11.7 (#561475)

---
+++ pkgs/development/python-modules/imap-tools/default.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "imap-tools";
+  version = "1.13.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ikvk";
+    repo = "imap_tools";
+    tag = "v${version}";
+    hash = "sha256-1BcSF40yUbOvOVDsoqS4AXRU2GZ1a3f9p8Xz7crsfwc=";
+  };
+
+  build-system = [ setuptools ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  disabledTests = [
+    # tests require a network connection
+    "test_action"
+    "test_attributes"
+    "test_connection"
+    "test_folders"
+    "test_idle"
+    "test_live"
+    # broken on Python 3.14.7
+    # reported upstream: https://github.com/ikvk/imap_tools/issues/271
+    "test_login_quotes_plain_username"
+    "test_login_quotes_username_with_special_chars"
+  ];
+
+  pythonImportsCheck = [ "imap_tools" ];
+
+  meta = {
+    description = "Work with email and mailbox by IMAP";
+    homepage = "https://github.com/ikvk/imap_tools";
+    changelog = "https://github.com/ikvk/imap_tools/blob/${src.tag}/docs/release_notes.rst";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ dotlambda ];
+  };
+}
